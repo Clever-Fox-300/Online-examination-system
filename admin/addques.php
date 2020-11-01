cc96df21474957b57443836ba375da9f626a9a9e
Pawan300 <pawanbisht300@gmail.com>
2020-11-01 16:17:13 +0530

    Updating files

---
+++ admin/addques.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Question Type</title>
+        <link rel="stylesheet" type="text/css"  href="qstyle.css">
+</head>
+<body>
+<div class="header">
+        <h2>Question Type</h2>
+    </div>
+
+    
+</body>
+<form method="post" action="schedule.php">
+<div class="input-group">
+    <button type="submit" name="ques" class="btn">Objective</button> 
+    </div>
+    <div class="input-group">
+    <button type="submit" name="ques" class="btn">Subjective</button> 
+    </div>
+</form>
+</body>
+</html>
